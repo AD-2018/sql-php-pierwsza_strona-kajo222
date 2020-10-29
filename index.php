cc037760e79e420b1fabdf54c0650678f62e9222
Kacper Korczak <kacper.korczak123@gmail.com>
2020-10-29 14:32:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,17 @@ $conn = new mysqli("sql7.freemysqlhosting.net","sql7373393","jbaItm9QJv","sql737
 $sql = "SELECT * FROM pracownicy";
 $wynik = mysqli_query($conn, $sql);
 
+    
+    echo('<table border="1">');
+    echo('<th>Imie</th><th>zarobki</th>');
+
     while($wiersz=mysqli_fetch_assoc($wynik)){
-        echo($wiersz['imie']);
+        echo('<tr>');
+        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
+        echo('</tr>');
     }
+
+    echo('</table>');
 
 
 ?>
