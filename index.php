f50e1a68a8acc72125a0c9dda19aeadb0f8b3641
Kacper Korczak <kacper.korczak123@gmail.com>
2020-11-05 13:59:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,25 @@
-<?php
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-kajo222">github</a>
+        <h1>Kacper Korczak</h1>
 
-include 'connect.php';
+            <?php
 
-    
-    echo('<table border="1">');
-    echo('<th>Imie</th><th>zarobki</th>');
+            include 'connect.php';
+                echo('<table border="1">');
+                    echo('<th>Imie</th><th>zarobki</th>');
+                        while($wiersz=mysqli_fetch_assoc($wynik)){
+                            echo('<tr>');
+                            echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
+                            echo('</tr>');
+                        }
+                echo('</table>');
+            ?>
+     </body>
+</html>
 
-    while($wiersz=mysqli_fetch_assoc($wynik)){
-        echo('<tr>');
-        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
-        echo('</tr>');
-    }
 
-    echo('</table>');
-
-
-?>
+           
