aec3f5336b47b244872e134fffb7d741b715f61c
Kacper Korczak <kacper.korczak123@gmail.com>
2020-11-05 14:31:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,8 +10,7 @@
             require_once("connect.php");
 
                 echo("<h3>tabelka</h3>");
-                echo('<table border="1" class="tabela"');
-                    echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>"); 
+                    echo('<table border="1" class="tabela"');echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>"); 
                         while($wiersz=mysqli_fetch_assoc($wynik)){
                             echo("<tr>");   
                             echo("<tr>");
@@ -20,27 +19,66 @@
                             echo("</tr>");
                         } 
                     echo('</table>');
+                
+            $sql ="select avg(zarobki),nazwa_dzial from pracownicy,organizacja where id_org=dzial and imie not like '%a' group by dzial having avg(zarobki)<35"; 
+            echo("<h3>zadanie1</h3>"); 
+            $wynik = mysqli_query($conn, $sql);
+
+                echo('<table border="1" class="tabela"'); 
+                    echo ("<tr><th>avg(zarobki)</th><th>nazwa_dzial</th></tr>"); 
+                        while($wiersz=mysqli_fetch_assoc($wynik)){ 
+                            echo("<tr>");     
+                            echo("<tr>");     
+                            echo("<td>".$wiersz['avg(zarobki)']."</td><td>".$wiersz['nazwa_dzial']."</td>");     
+                            echo("<tr>"); 
+                            echo("</tr>");
+                        }
+                echo('</table>'); 
             
-        
-        
+            $sql ="select avg(zarobki),nazwa_dzial from pracownicy,organizacja where id_org=dzial group by dzial having avg(zarobki)<40"; 
+            echo("<h3>zadanie2</h3>"); 
+            $wynik = mysqli_query($conn, $sql);  
+
+                echo('<table border="1" class="tabela"'); 
+                    echo ("<tr><th>avg(zarobki)</th><th>nazwa_dzial</th></tr>"); 
+                        while($wiersz=mysqli_fetch_assoc($wynik)){ 
+                            echo("<tr>");     
+                            echo("<tr>");     
+                            echo("<td>".$wiersz['avg(zarobki)']."</td><td>".$wiersz['nazwa_dzial']."</td>");
+                            echo("<tr>");
+                            echo("</tr>");
+                        }
+                echo('</table>');
+            
+            $sql ="select imie,zarobki from pracownicy where imie not like '%a' having zarobki<40";
+            echo("<h3>zadanie3</h3>");
+            $wynik = mysqli_query($conn, $sql);
+
+                echo('<table border="1" class="tabela"');
+                    echo ("<tr><th>imie</th><th>zarobki</th></tr>");
+                        while($wiersz=mysqli_fetch_assoc($wynik)){
+                            echo("<tr>");
+                            echo("<tr>");
+                            echo("<td>".$wiersz['imie']."</td><td>".$wiersz['zarobki']."</td>");
+                            echo("<tr>");
+                            echo("</tr>");
+                        } 
+                echo('</table>');
+            
             $sql ="select nazwa_dzial from pracownicy,organizacja where id_org=dzial and (dzial=2 or dzial=3) group by dzial";
-            echo("<h3>zadanie1</h3>");
+            echo("<h3>zadanie4</h3>");
             $wynik = mysqli_query($conn, $sql);  
 
                 echo('<table border="1" class="tabela"');
-                    echo ("<th>nazwa_dzial</th>");
+                    echo ("<tr><th>nazwa_dzial</th></tr>");
                         while($wiersz=mysqli_fetch_assoc($wynik)){
                             echo("<tr>");
-                            echo("<td>".$wiersz['nazwa_dzial']."</td>");
-                            echo("</tr>");
                             echo("<tr>");
                             echo("<td>".$wiersz['nazwa_dzial']."</td>");
+                            echo("<tr>");
                             echo("</tr>");
                         } 
                     echo('</table>'); 
-       
-                echo('</table>'); 
-        
             ?>
     </body>
 </html>
