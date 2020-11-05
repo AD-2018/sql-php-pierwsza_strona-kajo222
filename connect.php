3eeb194b64c27861e27920830b5c64f10edb0bc7
Kacper Korczak <kacper.korczak123@gmail.com>
2020-11-05 13:35:38 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("mysql-kako2307.alwaysdata.net","kako2307_11","QWE!@#123","kako2307_1");
+$conn = new mysqli("mysql-kako2307.alwaysdata.net","kako2307_1","QWE!@#123","kako2307_1");
 $sql = "SELECT * FROM pracownicy";
 $wynik = mysqli_query($conn, $sql);
 ?>
