2910a5efa877553da2b47b0dcdda5318a53d0fcf
Kacper Korczak <kacper.korczak123@gmail.com>
2020-11-06 11:47:30 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
             require_once("connect.php");
 
                 echo("<h3>tabelka</h3>");
-                    echo('<table border="1" class="tabela"');echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>"); 
+                    echo('<table border="1" class="tabela"');
+                    echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>"); 
                         while($wiersz=mysqli_fetch_assoc($wynik)){ 
                             echo("<tr>");
                             echo("<td>".$wiersz['id_pracownicy']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['dzial']."</td><td>".$wiersz['zarobki']."</td><td>".$wiersz['data_urodzenia']."</td>");     
@@ -24,7 +25,7 @@
             $wynik = mysqli_query($conn, $sql);
 
                 echo('<table border="1" class="tabela"');
-                    echo ("<tr><th>imie</th><th>zarobki</th></tr>");
+                    echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>");
                         while($wiersz=mysqli_fetch_assoc($wynik)){
                             echo("<tr>");
                             echo("<td>".$wiersz['id_pracownicy']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['dzial']."</td><td>".$wiersz['zarobki']."</td><td>".$wiersz['data_urodzenia']."</td>");
@@ -38,7 +39,7 @@
             echo("<h3>Zadanie 2</h3>");
             $wynik = mysqli_query($conn, $sql);  
                 echo('<table border="1" class="tabela"');
-                    echo ("<tr><th>nazwa_dzial</th></tr>");
+                    echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>");
                         while($wiersz=mysqli_fetch_assoc($wynik)){
                             echo("<tr>");
                             echo("<td>".$wiersz['id_pracownicy']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['dzial']."</td><td>".$wiersz['zarobki']."</td><td>".$wiersz['data_urodzenia']."</td>");
