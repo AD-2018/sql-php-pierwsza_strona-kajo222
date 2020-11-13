b350bf28a98ae2529ece637b71930d264a0c746a
Kacper Korczak <kacper.korczak123@gmail.com>
2020-11-13 12:54:04 +0100

    Update dt.php

--- dt.php
+++ dt.php
@@ -3,7 +3,7 @@
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-       <h1><table class="nav"><tr><td><button class="button" onclick="window.location.href='https://github.com/AD-2018/sql-php-pierwsza_strona-kajo222'">Github - Kacper Korczak</button></td><td><a href="index.php">Strona Główna</a></td><td><a href="oip.php">Pracownicy i Organizacja </a></td><td><a href="fa.php">Funkcje agregujące </a></td><td><a href="dt.php">Data i Czas </a></td></tr></table></h1>
+       <h1><table class="nav"><tr><td><button class="button" onclick="window.location.href='https://github.com/AD-2018/sql-php-pierwsza_strona-kajo222'">Github - Kacper Korczak</button></td><td><button class="button" onclick="window.location.href='index.php'">Strona Główna</button></td><td><button class="button" onclick="window.location.href='oip.php'">Pracownicy i Organizacja</button></td><td><button class="button" onclick="window.location.href='fa.php'">Funkcje Agregujące</button></td><td><a href="dt.php">Data i Czas </a></td></tr></table></h1>
         <?php
             require_once("connect.php");
 
