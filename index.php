7246d74ed96186382aa4c34085cfa3b9d699f926
Kacper Korczak <kacper.korczak123@gmail.com>
2020-11-13 10:48:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,8 @@
 
 
 
-                $sql = "SELECT * FROM pracownicy where zarobki<30";    
+                $sql = "SELECT * FROM pracownicy where zarobki<30";   
+                $result = mysqli_query($conn, $sql);
                 echo("Zad 3 - ".$sql);
                     echo('<table border="1">');
                         echo('<th>Imie</th><th>zarobki</th><th>Dzial</th>');
@@ -48,17 +49,6 @@
 
 
 
-                $sql = "SELECT * FROM pracownicy where dzial=2";
-                $result = mysqli_query($conn, $sql);
-                echo("Zad 1 - ".$sql);
-                    echo('<table border="1">');
-                        echo('<th>Imie</th><th>zarobki</th><th>Dzial</th>');
-                            while($row=mysqli_fetch_assoc($result)){
-                                echo('<tr>');
-                                echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td><td>'.$row['dzial'].'</td>');
-                                echo('</tr>');
-                            }
-                    echo('</table>');
 
 
         ?>
