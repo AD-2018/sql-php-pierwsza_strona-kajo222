17ee389ce773f6ccc4d38384fce39f793b529e88
Kacper Korczak <kacper.korczak123@gmail.com>
2020-11-13 10:33:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,66 +3,61 @@
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-       <h1><button class="button" onclick="window.location.href='https://github.com/AD-2018/sql-php-pierwsza_strona-kajo222'">Github - Kacper Korczak</button></h1>
-
-            <?php
+       <h1><button class="button" onclick="window.location.href='https://github.com/AD-2018/sql-php-pierwsza_strona-kajo222'">Github - Kacper Korczak</button><a href="oip.php">Pracownicy i Organizacja </a><a href="fa.php">Funkcje agregujące </a><a href="dt.php">Data i Czas </a></h1>
+        <?php
             require_once("connect.php");
 
-                echo("<h3>tabelka</h3>");
-                    echo('<table border="1" class="tabela"');
-                    echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>"); 
-                        while($wiersz=mysqli_fetch_assoc($wynik)){ 
-                            echo("<tr>");
-                            echo("<td>".$wiersz['id_pracownicy']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['dzial']."</td><td>".$wiersz['zarobki']."</td><td>".$wiersz['data_urodzenia']."</td>");     
-                            echo("</tr>");
-                        } 
+                $sql = "SELECT * FROM pracownicy where dzial=2";    
+                echo("Zad 1 - ".$sql);
+                    echo('<table border="1">');
+                        echo('<th>Imie</th><th>zarobki</th><th>Dzial</th>');
+                            while($row=mysqli_fetch_assoc($result)){
+                                echo('<tr>');
+                                echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td><td>'.$row['dzial'].'</td>');
+                                echo('</tr>');
+                            }
                     echo('</table>');
-                
 
-        
-            $sql ="SELECT  * FROM pracownicy WHERE dzial=2";
-            echo("<h3>Zadanie 1</h3>");
-            $wynik = mysqli_query($conn, $sql);
 
-                echo('<table border="1" class="tabela"');
-                    echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>");
-                        while($wiersz=mysqli_fetch_assoc($wynik)){
-                            echo("<tr>");
-                            echo("<td>".$wiersz['id_pracownicy']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['dzial']."</td><td>".$wiersz['zarobki']."</td><td>".$wiersz['data_urodzenia']."</td>");
-                            echo("</tr>");
-                        } 
-                echo('</table>');
-        
-        
-                     
-            $sql ="SELECT  * FROM pracownicy WHERE dzial=2 or dzial=3"; 
-            echo("<h3>Zadanie 2</h3>");
-            $wynik = mysqli_query($conn, $sql);
-                echo('<table border="1" class="tabela"'); 
-                    echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>");
-                        while($wiersz=mysqli_fetch_assoc($wynik)){  
-                            echo("<tr>"); 
-                            echo("<td>1</td>";
-                            echo("<td>".$wiersz['id_pracownicy']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['dzial']."</td><td>".$wiersz['zarobki']."</td><td>".$wiersz['data_urodzenia']."</td>");
-                            echo("</tr>");
-                        }
-                echo('</table>'); 
-        
-        
-        
-            $sql ="SELECT  * FROM pracownicy WHERE zarobki<30"; 
-            echo("<h3>Zadanie 3</h3>");
-            $wynik = mysqli_query($conn, $sql);
-                echo('<table border="1" class="tabela"'); 
-                    echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>");
-                        while($wiersz=mysqli_fetch_assoc($wynik)){  
-                            echo("<tr>");     
-                            echo("<td>".$wiersz['id_pracownicy']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['dzial']."</td><td>".$wiersz['zarobki']."</td><td>".$wiersz['data_urodzenia']."</td>");
-                            echo("</tr>");
-                        }
-                echo('</table>'); 
-   
 
-            ?>
+                $sql = "SELECT * FROM pracownicy where (dzial=2) or (dzial=3)";    
+                echo("Zad 2 - ".$sql);
+                    echo('<table border="1">');
+                        echo('<th>Imie</th><th>zarobki</th><th>Dzial</th>');
+                            while($row=mysqli_fetch_assoc($result)){
+                                echo('<tr>');
+                                echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td><td>'.$row['dzial'].'</td>');
+                                echo('</tr>');
+                            }
+                    echo('</table>');
+
+
+
+                $sql = "SELECT * FROM pracownicy where zarobki<30";    
+                echo("Zad 3 - ".$sql);
+                    echo('<table border="1">');
+                        echo('<th>Imie</th><th>zarobki</th><th>Dzial</th>');
+                            while($row=mysqli_fetch_assoc($result)){
+                                echo('<tr>');
+                                echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td><td>'.$row['dzial'].'</td>');
+                                echo('</tr>');
+                            }
+                    echo('</table>');
+
+
+
+                $sql = "SELECT * FROM pracownicy where dzial=2";    
+                echo("Zad 1 - ".$sql);
+                    echo('<table border="1">');
+                        echo('<th>Imie</th><th>zarobki</th><th>Dzial</th>');
+                            while($row=mysqli_fetch_assoc($result)){
+                                echo('<tr>');
+                                echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td><td>'.$row['dzial'].'</td>');
+                                echo('</tr>');
+                            }
+                    echo('</table>');
+
+
+        ?>
     </body>
 </html>
