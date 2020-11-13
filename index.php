f96f58da0c9382bb4f3d77464d7e81572975937e
Kacper Korczak <kacper.korczak123@gmail.com>
2020-11-13 10:43:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-       <h1><button class="button" onclick="window.location.href='https://github.com/AD-2018/sql-php-pierwsza_strona-kajo222'">Github - Kacper Korczak</button><a href="oip.php">Pracownicy i Organizacja </a><a href="fa.php">Funkcje agregujące </a><a href="dt.php">Data i Czas </a></h1>
+       <h1><button class="button" onclick="window.location.href='https://github.com/AD-2018/sql-php-pierwsza_strona-kajo222'">Github - Kacper Korczak</button><table><tr><td><a href="oip.php">Pracownicy i Organizacja </a></td><td><a href="fa.php">Funkcje agregujące </a></td><td><a href="dt.php">Data i Czas </a></td></tr></h1>
         <?php
             require_once("connect.php");
 
