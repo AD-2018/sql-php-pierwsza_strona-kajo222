e036d2fa2896c63e04666bed8f28e14cfe258291
Kacper Korczak <kacper.korczak123@gmail.com>
2020-11-13 10:41:22 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 $conn = new mysqli("mysql-kako2307.alwaysdata.net","kako2307_1","QWE!@#123","kako2307_1");
 $sql = "SELECT * FROM pracownicy";
-$wynik = mysqli_query($conn, $sql);
+$result = mysqli_query($conn, $sql);
 
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
