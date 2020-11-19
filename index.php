59c11ce997ab10f44dc7a4c62823359bb9d900b9
Kacper Korczak <kacper.korczak123@gmail.com>
2020-11-19 14:29:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-       <h1><table class="nav"><tr><td><button class="button" onclick="window.location.href='https://github.com/AD-2018/sql-php-pierwsza_strona-kajo222'">Github - Kacper Korczak</button></td><td><button class="button" onclick="window.location.href='index.php'">Strona Główna</button></td><td><button class="button" onclick="window.location.href='oip.php'">Pracownicy i Organizacja</button></td><td><button class="button" onclick="window.location.href='fa.php'">Funkcje Agregujące</button></td><td><button class="button" onclick="window.location.href='dt.php'">Data i Czas</button></td></tr></table></h1>
         <?php
+            include("navbar.php");
             require_once("connect.php");
 
                 $sql = "SELECT * FROM pracownicy where dzial=2";
