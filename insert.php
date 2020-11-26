2f22b1595e742df77e0dbcf48f634f5ac8b3723d
Kacper Korczak <kacper.korczak123@gmail.com>
2020-11-26 14:30:53 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -5,15 +5,7 @@ echo "<li>". $_POST['dzial'];
 echo "<li>". $_POST['zarobki'];
 echo "<li>". $_POST['data_urodzenia'];
 
-$servername = "mysql-dominikaszerlag.alwaysdata.net";
-$username = "217134";
-$password = "@dominika123";
-$dbname = "dominikaszerlag_baza";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+require_once("connect.php");
 
 $sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
        VALUES (null, '".$_POST['imie']."', '".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_urodzenia']."')";
