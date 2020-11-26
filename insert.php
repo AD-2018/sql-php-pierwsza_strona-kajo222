9a560405645255e011457e7681593094d59291f2
Kacper Korczak <kacper.korczak123@gmail.com>
2020-11-26 14:25:40 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,11 +1,24 @@
 <?php
-$conn = new mysqli("mysql-kako2307.alwaysdata.net","kako2307_1","QWE!@#123","kako2307_1");
+echo("jestes w insert.php <br>");
+echo "<li>". $_POST['imie'];
+echo "<li>". $_POST['dzial'];
+echo "<li>". $_POST['zarobki'];
+echo "<li>". $_POST['data_urodzenia'];
+
+$servername = "mysql-dominikaszerlag.alwaysdata.net";
+$username = "217134";
+$password = "@dominika123";
+$dbname = "dominikaszerlag_baza";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
 $sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
-       VALUES (null, $_POST['imie'], 1, 76,'1991-11-21')";
+       VALUES (null, '".$_POST['imie']."', '".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_urodzenia']."')";
+
+echo "<li>". $sql;
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
