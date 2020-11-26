4e76bd9fef1404db8fd902876c2ad5b2b4bf6fda
Kacper Korczak <kacper.korczak123@gmail.com>
2020-11-26 14:28:34 +0100

    Update insertbaza.php

--- insertbaza.php
+++ insertbaza.php
@@ -1,14 +1,25 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width">
-  <title>Dane do Bazy</title>
-</head>
-<body>
-	<form action="insertbaza.php" method="POST">
-	    <input type="text" name="imie">
-	    <input type="submit">
-	</form>
-</body>
+	<head>
+		  <meta charset="utf-8">
+		  <meta name="viewport" content="width=device-width">
+		  <title>Dane do Bazy</title>
+	</head>
+	<body>
+		<h3>DODAJ PRACOWNIKA</h3>
+		<form action="insert.php" method="POST">
+			<input type="text" name="imie">
+			<input type="number" name="dzial"></br>
+			<input type="number" name="zarobki"></br>
+			<input type="date" name="data_urodzenia"></br>
+			<input type="submit" value="dodaj pracownika">
+		</form>
+		<br />
+		<h3>USUN PRACOWNIKA</h3>
+		<form action="delete.php" method="POST">
+			<input type="number" name="id"></br>
+			<input type="submit" value="Zapisz w zmiennej $_POST['id']">
+		</form>
+
+	</body>
 </html>
