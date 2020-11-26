dc1e013587c321928cd23f27e880b02b4cbd5243
Kacper Korczak <kacper.korczak123@gmail.com>
2020-11-26 14:34:16 +0100

    Update insertbaza.php

--- insertbaza.php
+++ insertbaza.php
@@ -8,16 +8,16 @@
 	<body>
 		<h3>DODAJ PRACOWNIKA</h3>
 		<form action="insert.php" method="POST">
-			<input type="text" name="imie">
-			<input type="number" name="dzial"></br>
-			<input type="number" name="zarobki"></br>
-			<input type="date" name="data_urodzenia"></br>
+			<input type="text" name="imie" placeholder="imie">
+			<input type="number" name="dzial" placeholder="dzial"><br />
+			<input type="number" name="zarobki" placeholder="zarobki"><br />
+			<input type="date" name="data_urodzenia" placeholder="data urodzenia"><br />
 			<input type="submit" value="dodaj pracownika">
 		</form>
 		<br />
 		<h3>USUN PRACOWNIKA</h3>
 		<form action="delete.php" method="POST">
-			<input type="number" name="id"></br>
+			<input type="number" name="id" placeholder="id"><br />
 			<input type="submit" value="Zapisz w zmiennej $_POST['id']">
 		</form>
 
