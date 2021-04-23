41a947620f17a9ed0a61a4bb33a7fb6ba3fc025d
Kacper Korczak <kacper.korczak123@gmail.com>
2021-04-23 10:46:52 +0200

    dlt

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = new mysqli($_SERVER["servername"],$_SERVER["user"],$_SERVER["pass"],$_SERVER["dbname"]);
-$sql = "SELECT * FROM pracownicy";
-
-
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-?>
--- strona.php
+++
@@ -1,9 +0,0 @@
-<?php
-	echo("jesteś na stronie.php");
-	
-	echo("<ul>");
-	
-	echo("<li>".$_POST["firstname"]);
-		
-	echo("<ul>");
-?>
