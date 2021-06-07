48371e04bf2ca85a2c86793fd2c32e4d9aef7e12
Kacper Korczak <kacper.korczak123@gmail.com>
2021-06-07 09:29:16 +0200

    Update strona9.php

--- grid/strona9.php
+++ grid/strona9.php
@@ -6,93 +6,31 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="Grid.css">
 </head>
-
 <body>
     <strong>
     <div class="str9">
         <div class="str9A">
-        <?php
-                require_once("../connect.php");
-                $sql = "SELECT * FROM Osoby";
-                
-                    if ($conn->connect_error) {
-                        die("Connection failed: " . $conn->connect_error);
-                      }
-                
-                    $result = mysqli_query($conn, $sql);
-                    if ( $result) {
-                         echo "<br>";
-                     } else {
-                       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-                     }
-                
-                    echo("<h1>Pracownicy</h1>");
-                
-                    echo("<table border='1'>");
-                    echo("<th>ID</th><th>Imię i Nazwisko</th>");
-                        while($row = mysqli_fetch_assoc($result)) {
-                            echo("<tr>");
-                            echo("<td>".$row['ID']."</td><td>".$row['Imie-Nazwisko']."</td>");
-                            echo("</tr>");
-                        };
-                    echo("</table>");
-                    echo ("<br>");
+            <h1>Pracownicy</h1>
+            <?php
+            require_once("funkcje.php");
+            tab_del_male("Osoby", "2", "id", "Imie_Nazwisko");
+            id_del_male("Osoby", "2");
             ?>
         </div>
         <div class="str9B">
-        <?php
-                require_once("../connect.php");
-                $sql = "SELECT * FROM projekt";
-                
-                    if ($conn->connect_error) {
-                        die("Connection failed: " . $conn->connect_error);
-                      }
-                
-                    $result = mysqli_query($conn, $sql);
-                    if ( $result) {
-                         echo "<br>";
-                     } else {
-                       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-                     }
-                
-                    echo("<h1>Projekty</h1>");
-                
-                    echo("<table border='1'>");
-                    echo("<th>ID</th><th>Projekt</th>");
-                        while($row = mysqli_fetch_assoc($result)) {
-                            echo("<tr>");
-                            echo("<td>".$row['id']."</td><td>".$row['projekt']."</td>");
-                            echo("</tr>");
-                        };
-                    echo("</table>");
-                    echo ("<br>");
+            <h1>Projekty</h1>
+            <?php
+            require_once("funkcje.php");
+            tab_del_male("projekt", "1", "id", "projekt");
+            id_del_male("projekt", "1");
             ?>
         </div>
         <div class="str9C">
         <?php
-                        require_once("../connect.php");
-                        $sql = "select projekt, `Imie-Nazwisko`, (`WDW`.ID) as ID_TAB from `kako2307_1`.WDW, `kako2307_1`.projekt, `kako2307_1`.Osoby where Osoby.ID=osoby_id and projekt.id=klasa_id order by ID_TAB asc";
-                            if ($conn->connect_error) {
-                                    die("Connection failed: " . $conn->connect_error);
-                             }
-                                $result = mysqli_query($conn, $sql);
-                            if ( $result) {
-                            } else {
-                                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-                            }
-                        
-                            echo("<h1>Wiele do Wielu</h1>");
-                        
-                            echo("<table border='1'>");
-                            echo("<th>ID</th><th>Projekt</th><th>Pracownik</th>");
-                                while($row = mysqli_fetch_assoc($result)) {
-                                    echo("<tr>");
-                                    echo("<td>".$row['ID_TAB']."</td><td>".$row['projekt']."</td><td>".$row['Imie-Nazwisko']."</td>");
-                                    echo("</tr>");
-                                };
-                            echo("</table>");
-                            echo ("<br>");
-                ?>
+                require_once("funkcje.php");
+                niefajne("WDW", "projekt", "Osoby", "id", "dane_1", "dane_2", "projekt", "Imie_Nazwisko");
+                id_del_duze("WDW");
+            ?>
         </div>
         <div class="str9D">4</div>
         <div class="str9E">Firma</div>
